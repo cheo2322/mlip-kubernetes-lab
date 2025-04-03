b4ef69ccc631b42fc93ab13606a13bb35a95ec15
Sergio Hidalgo <barcecheo2319@gmail.com>
2025-04-02 19:44:55 -0500

    Step 1 TODOS

--- backend.py
+++ backend.py
@@ -9,8 +9,7 @@ def hello_user():
     user_id = request.args.get("user_id", "Guest")
     instance = socket.gethostname()
     
-    return f"Hello, {user_id}! This response is from instance:" #Use the above defined instance here 
+    return f"Hello, {user_id}! This response is from instance: {instance}"
 
 if __name__ == '__main__':
-    # TODO: Change the port if needed (default is 5001)
     app.run(host='0.0.0.0', port=5001)
